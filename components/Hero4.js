33ac1b250900ba555a2ab02e533e8e8165760b15
Juan F Gonzalez <juano_gonza@hotmail.com>
2021-01-31 13:52:45 -0500

    Add assets and formatting

--- components/Hero4.js
+++ components/Hero4.js
@@ -3,24 +3,24 @@ import Section from "../components/Section";
 import SectionHeader from "../components/SectionHeader";
 
 function Hero4(props) {
-    return (
-        <Section
-            color={props.color}
-            size={props.size}
-            backgroundImage={props.backgroundImage}
-            backgroundImageOpacity={props.backgroundImageOpacity}
-        >
-            <div className="container">
-                <SectionHeader 
-                    title={props.title}
-                    subtitle={props.subtitle}
-                    size={3}
-                    spaced={true}
-                    className="has-text-centered"
-                />
-            </div>
-        </Section>
-    );
+  return (
+    <Section
+      color={props.color}
+      size={props.size}
+      backgroundImage={props.backgroundImage}
+      backgroundImageOpacity={props.backgroundImageOpacity}
+    >
+      <div className="container">
+        <SectionHeader
+          title={props.title}
+          subtitle={props.subtitle}
+          size={3}
+          spaced={true}
+          className="has-text-centered"
+        />
+      </div>
+    </Section>
+  );
 }
 
 export default Hero4;
